3ddcd1db7bbff1258279bae094a4f68befe959a2
Flash-X-Tech <dhiraj.flash.official@gmail.com>
2025-04-15 18:04:37 +0530

    Update YinAudio.vue

--- music-manage/src/components/layouts/YinAudio.vue
+++ music-manage/src/components/layouts/YinAudio.vue
@@ -16,23 +16,23 @@ export default defineComponent({
       divRef.value = el;
     };
 
-    const url = computed(() => store.getters.url); // 音乐链接
-    const isPlay = computed(() => store.getters.isPlay); // 播放状态
-    // 监听播放还是暂停
+    const url = computed(() => store.getters.url); 
+    const isPlay = computed(() => store.getters.isPlay); 
+ 
     watch(isPlay, () => {
       togglePlay();
     });
 
-    // 开始/暂停
+
     function togglePlay() {
       isPlay.value ? divRef.value.play() : divRef.value.pause();
     }
 
-    // 获取歌曲链接后准备播放
+
     function startPlay() {
       divRef.value.play();
     }
-    // 音乐播放结束时触发
+  
     function ended() {
       proxy.$store.commit("setIsPlay", false);
     }
