48c02df7d2b6eb8df3a8972b1fe3bbf2364facdf
Flash-X-Tech <dhiraj.flash.official@gmail.com>
2025-04-15 17:59:22 +0530

    Update request.ts

--- music-manage/src/api/request.ts
+++ music-manage/src/api/request.ts
@@ -3,28 +3,27 @@ import router from '../router'
 
 const BASE_URL = process.env.NODE_HOST
 
-axios.defaults.timeout = 5000 // 超时时间设置
-axios.defaults.withCredentials = true // true允许跨域
+axios.defaults.timeout = 5000
+axios.defaults.withCredentials = true
 axios.defaults.baseURL = BASE_URL
-// Content-Type 响应头
+
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8'
 
-// 响应拦截器
+
 axios.interceptors.response.use(
   response => {
-    // 如果返回的状态码为200，说明接口请求成功，可以正常拿到数据
-    // 否则的话抛出错误
+  
     if (response.status === 200) {
       return Promise.resolve(response)
     } else {
       return Promise.reject(response)
     }
   },
-  // 服务器状态码不是2开头的的情况
+
   error => {
     if (error.response.status) {
       switch (error.response.status) {
-        // 401: 未登录
+       
         case 401:
           router.replace({
             path: "/",
@@ -34,8 +33,7 @@ axios.interceptors.response.use(
           });
           break;
         case 403:
-          // console.log('管理员权限已修改请重新登录')
-          // 跳转登录页面，并将要浏览的页面fullPath传过去，登录成功后跳转需要访问的页面
+    
           setTimeout(() => {
             router.replace({
               path: "/",
@@ -46,9 +44,9 @@ axios.interceptors.response.use(
           }, 1000);
           break;
 
-        // 404请求不存在
+  
         case 404:
-          // console.log('请求页面飞到火星去了')
+        
           break;
       }
       return Promise.reject(error.response);
@@ -61,7 +59,7 @@ export function getBaseURL() {
 }
 
 /**
- * 封装get方法
+
  * @param url
  * @param data
  * @returns {Promise}
@@ -76,7 +74,7 @@ export function get(url, params?: object) {
 }
 
 /**
- * 封装post请求
+
  * @param url
  * @param data
  * @returns {Promise}
@@ -91,7 +89,7 @@ export function post(url, data = {}) {
 }
 
 /**
- * 封装delete请求
+
  * @param url
  * @param data
  * @returns {Promise}
@@ -106,7 +104,7 @@ export function deletes(url, data = {}) {
 }
 
 /**
- * 封装put请求
+
  * @param url
  * @param data
  * @returns {Promise}
